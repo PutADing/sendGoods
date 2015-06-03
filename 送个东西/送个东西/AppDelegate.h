d57e47e3dc3ce6d384c833cb81123e0141677d7c
PutADing <jiangkuan@jiangkuandeMacBook-Pro.local>
2015-06-03 16:52:12 +0800

    original
    20150603

---
+++ 送个东西/送个东西/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  送个东西
+//
+//  Created by 蒋宽 on 15/6/3.
+//  Copyright (c) 2015年 蒋宽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 送个东西/送个东西/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  送个东西
+//
+//  Created by 蒋宽 on 15/6/3.
+//  Copyright (c) 2015年 蒋宽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
